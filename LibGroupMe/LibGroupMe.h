1ee48b8f47658e056d007ee89525455b95d06b8f
Jon Balbarin <jonbalbarin@gmail.com>
2015-06-16 10:05:44 -0400

    Initial Commit

---
+++ LibGroupMe/LibGroupMe.h
@@ -0,0 +1,19 @@
+//
+//  LibGroupMe.h
+//  LibGroupMe
+//
+//  Created by Jon Balbarin on 6/16/15.
+//  Copyright (c) 2015 Jon Balbarin. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LibGroupMe.
+FOUNDATION_EXPORT double LibGroupMeVersionNumber;
+
+//! Project version string for LibGroupMe.
+FOUNDATION_EXPORT const unsigned char LibGroupMeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibGroupMe/PublicHeader.h>
+
+

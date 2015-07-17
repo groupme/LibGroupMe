1325bf7b3c1c9fd8860b4241c63f4f002a7b8712
Jon Balbarin <jonbalbarin@gmail.com>
2015-07-17 13:45:10 -0400

    add a bridging header

---
+++ LibGroupMe/LibGroupMe-swift.h
@@ -0,0 +1,13 @@
+//
+//  LibGroupMe-swift.h
+//  LibGroupMe
+//
+//  Created by Jon Balbarin on 7/16/15.
+//  Copyright (c) 2015 Jon Balbarin. All rights reserved.
+//
+
+#ifndef LibGroupMe_LibGroupMe_swift_h
+#define LibGroupMe_LibGroupMe_swift_h
+
+
+#endif
